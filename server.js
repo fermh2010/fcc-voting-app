c1f60337bbb2dc9c8973aa35e9490cdc4e6572f8
Fernando Herrera <fherrera@qb9.net>
2016-11-29 14:23:55 -0300

    refactor

--- server.js
+++ server.js
@@ -1,18 +1,44 @@
 const express = require('express');
 const app = express();
 
-require('lasso').configure('./lasso-config.json');
+require('lasso').configure('./client/lasso-config.json');
 
 require('marko/node-require').install();
 require('marko/compiler').defaultOptions.writeToDisk = false;
 const pages = {
-  index: require('./src/index.marko')
+  index: require('./client/index.marko')
 }
 
-app.use('/static', express.static('static'));
+const providers = {
+  polls: function() {
+    return new Promise(function(resolve, reject) {
+      resolve([ { title: 'Foo' }, { title: 'Bar'} ]);
+    });
+  }
+};
+
+const markoGlobals = {
+  providers: providers
+}
+
+app.use('/static', express.static('client/static'));
 
 app.get('/', function(req, res, next) {
-  pages.index.render({}, res);
+  pages.index.render({
+    $global: markoGlobals
+  }, res);
+});
+
+app.get('/login', function(req, res, next) {
+  res.end('TODO');
+});
+
+app.get('/user_polls', function(req, res, next) {
+  res.end('TODO');
+});
+
+app.get('/new_poll', function(req, res, next) {
+  res.end('TODO');
 });
 
 app.listen(process.env.PORT || 8080, function() {
