6c70a62449aebd499afb1a8b61c2f7bee5806afc
Fernando Herrera <fherrera@qb9.net>
2016-11-30 13:28:22 -0300

    implemented mongo db for creating and querying all and individual polls

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
+'use strict';
+
 const express = require('express');
 const app = express();
 
 const staticPath = "./client/static";
+
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+const MongoID = mongodb.ObjectID;
+const mongoURI = 'mongodb://fermh2010:123456@ds035177.mlab.com:35177/fcc_challenges';
 
 require('lasso').configure({
     "outputDir": staticPath,
@@ -24,9 +31,29 @@ const pages = {
 }
 
 const providers = {
-  allPolls: function() {
+  allPolls: function(_skip, _limit) {
     return new Promise(function(resolve, reject) {
-      resolve([ { title: 'Foo' }, { title: 'Bar'}, { title: 'Baz' } ]);
+      const skip = _skip || 0;
+      const limit = _limit || 10;
+      MongoClient.connect(mongoURI)
+      .then(db => {
+        const c = db.collection('polls');
+        c.find()
+        .skip(skip)
+        .limit(limit)
+        .toArray()
+        .then(arr => {
+          db.close();
+          resolve(arr);
+        })
+        .catch(err => {
+          db.close();
+          reject(err);
+        })
+      })
+      .catch(err => {
+        reject(err);
+      });
     });
   },
   userPolls: function() {
@@ -64,33 +91,76 @@ app.get('/new-poll', function(req, res, next) {
   pages.newPoll.render({}, res);
 });
 
-app.post('/new-poll', function(req, res, next) {
-  res.redirect('/user-polls');
+app.post('/new-poll', formParser, function(req, res, next) {
+  if(!req.body) {
+    console.log('ERROR: empty body for POST /new-poll'); // TODO: use production logger
+    return res.sendStatus(400);
+  }
+
+  const pollTitle = req.body['poll-title'];
+  const pollOptionsRaw = req.body['poll-options'];
+  if(!pollTitle || !pollOptionsRaw) {
+    console.log('ERROR: invalid parameters for POST /new-poll'); // TODO: use production logger
+    return res.sendStatus(400);
+  } else if(pollTitle.length == 0 || pollOptionsRaw.length == 0) {
+    console.log('ERROR: empty parameters for POST /new-poll'); // TODO: use production logger
+    return res.sendStatus(400);
+  }
+
+  const pollOptions = pollOptionsRaw.split(',').map(function(option) {
+    return option.trim();
+  });
+
+  MongoClient.connect(mongoURI)
+  .then(function(db) {
+    const c = db.collection('polls');
+    c.insertOne({
+      title: pollTitle,
+      options: pollOptions
+    })
+    .then(function() {
+      db.close();
+      res.redirect('/user-polls');
+    })
+    .catch(function(err){
+      db.close();
+      return next(err);
+    })
+  })
+  .catch(function(err) {
+    return next(err);
+  })
 });
 
 app.get('/polls/:pollId', function(req, res, next) {
-  const pollId = Number(req.params.pollId);
-  if(isNaN(pollId)) {
-    return next(new Error('invalid poll id'));
-  }
+  MongoClient.connect(mongoURI)
+  .then(db => {
+    const c = db.collection('polls');
+    c.findOne({ _id: new MongoID(req.params.pollId) })
+    .then(doc => {
+      db.close();
 
-  const poll = {
-    title: 'aasdasdasd asda sa sdasd?',
-    options: [
-      'option1',
-      'option2',
-      'option3'
-    ]
-  };
-
-  pages.poll.render({
-    poll: poll
-  }, res);
+      if(!doc) {
+        return res.status(200).send('poll not found');
+      } else {
+        pages.poll.render({
+          poll: doc
+        }, res);
+      }
+    })
+    .catch(err => {
+      db.close();
+      return next(err);
+    });
+  })
+  .catch(err => {
+    return next(err);
+  });
 });
 
 app.post('/polls/:pollId', formParser, function(req, res, next) {
   if(!req.body)
-    res.sendStatus(400);
+    return res.sendStatus(400);
 
   const option = req.body['selected-option'];
   if(option) {
